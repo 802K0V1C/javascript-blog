d19df04589295945526808001d8f6bda5b43b83f
Artur Bożek <arturhubert.bozek@gmail.com>
2023-01-19 19:13:17 +0100

    Change .titles for .article-1

--- js/script.js
+++ js/script.js
@@ -10,11 +10,19 @@ const titleClickHandler = function (event){
   console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
-
+  console.log('clickedElement:', clickedElement);
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.article-1 a.active');
 
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
