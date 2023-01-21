186f25efa78c977865e8c4bbf07d9d52731cc83d
Artur Bożek <arturhubert.bozek@gmail.com>
2023-01-21 12:47:11 +0100

    Add active for clicked links in left row

--- js/script.js
+++ js/script.js
@@ -6,8 +6,9 @@ document.getElementById('test-button').addEventListener('click', function(){
   });
 */
 const titleClickHandler = function (event){
-  console.log(event)
+  const clickedElement = this;
   console.log('Link was clicked!');
+  console.log(event);
 
   /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -17,8 +18,10 @@ const titleClickHandler = function (event){
   }
   /* add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
+  
+  clickedElement.classList.add('active');
   /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.article-1 a.active');
+  const activeArticles = document.querySelectorAll('.post .active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
