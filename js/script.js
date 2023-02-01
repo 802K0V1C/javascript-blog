317b414a43a30bca51cd0e8df9608ec102a054bb
Artur Bożek <arturhubert.bozek@gmail.com>
2023-02-01 20:08:12 +0100

    test

--- js/script.js
+++ js/script.js
@@ -152,7 +152,7 @@ function generateTags(){
       /* [NEW] START LOOP: for each tag in allTags: */
       for(let tag in allTags){
       /* [NEW] generate code of a link and add it to allTagsHTML */
-        allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+        allTagsHTML += '<a href="tag-' + tag +'">' + tag ' (' + allTags[tag] + ') ';
       }
       /* [NEW] END LOOP: for each tag in allTags: */
 
